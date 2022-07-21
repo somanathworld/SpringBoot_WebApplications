1b057a40bb3708b323e2a5e84244322778aefca9
TL <TL@gmail.com>
2022-07-21 16:09:38 +0530

    Spring Boot Using Thymeleaf

---
+++ Proj27-Boot_MVC_Thymeleaf/src/main/java/com/nt/service/EmployeeMgmtServiceImpl.java
@@ -0,0 +1,40 @@
+package com.nt.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nt.model.Employee;
+import com.nt.repository.IEmployeeRepo;
+
+@Service
+public class EmployeeMgmtServiceImpl implements IEmployeeMgmtService{
+	
+	@Autowired
+	private IEmployeeRepo repo;
+	
+	@Override
+	public Iterable<Employee> getAllEmployees() {
+		return repo.findAll();
+	}
+	
+	@Override
+	public String insertEmployee(Employee emp) {
+		return "Employee inserted with id value : "+repo.save(emp).getEmpno();
+	}
+
+	@Override
+	public Employee getEmployeeByEno(int eno) {
+		return repo.findById(eno).get();
+	}
+	
+	@Override
+	public String updateEmployee(Employee emp) {
+		return repo.save(emp).getEmpno()+" employee updated.";
+	}
+	
+	@Override
+	public String deleteEmployeeByEno(int eno) {
+		repo.deleteById(eno);
+		return eno+" employee number Employee deleted.";
+	}
+}
---
+++ Proj27-Boot_MVC_Thymeleaf/src/main/java/com/nt/service/IEmployeeMgmtService.java
@@ -0,0 +1,12 @@
+package com.nt.service;
+
+import com.nt.model.Employee;
+
+public interface IEmployeeMgmtService {
+
+	public Iterable<Employee> getAllEmployees();
+	public String insertEmployee(Employee emp);
+	public Employee getEmployeeByEno(int eno);
+	public String updateEmployee(Employee emp);
+	public String deleteEmployeeByEno(int eno);
+}
