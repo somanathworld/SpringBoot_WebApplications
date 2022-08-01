497d66d6a2e94e8097db6382d852eabf37f75be9
DELL <DELL@DESKTOP-EGFVMC6>
2022-08-01 10:01:15 +0530

    Spring Rest Mini Project

---
+++ Proj40_Boot_Rest_MiniProject/src/main/java/com/nt/Proj40BootRestMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj40BootRestMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj40BootRestMiniProjectApplication.class, args);
+	}
+
+}
